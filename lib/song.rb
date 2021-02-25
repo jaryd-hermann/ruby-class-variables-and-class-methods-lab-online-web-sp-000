e14341e167f7152d8adf3decf6948be7c4300298
Jaryd Hermann <hermannjaryd@gmail.com>
2021-02-25 22:47:59 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+class Song
+attr_accessor :count :artists :genres
+ @@count = 0
+
+def initialize(name, artist, genre)
+  #adding to class constant
+  @@count += 1
+  #giving new instance props
+  @name = name
+  @artist = artist
+  @genre = genre
+end
+
+#method to count songs
+def count
+  @@count
+end
+
+#method to return array of unique genres
+def genres
+  @@genres = []
+end
+
+
+def artists
+  @@artists = []
+end
+
+
+
+end
